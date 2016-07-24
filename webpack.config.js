7edce81dc25a9aa170bf9e68dc3fb15dd720ed2e
Kenneth Lee <kennyxlee@gmail.com>
2016-07-23 19:42:33 -0700

    Initial commit.

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    './src/Index.js',
+  ],
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'react-hot!babel' },
+    ],
+  },
+  output: {
+    path: 'dist',
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+};
