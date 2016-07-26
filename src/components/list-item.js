16c20351d087b722be5b31c14ad031a0f69211b2
Thomas Lo <thomasjinlo@gmail.com>
2016-07-26 09:13:05 -0700

    Change back to dumb component

--- src/components/list-item.js
+++ src/components/list-item.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Component } from 'react';
 import Radium from 'radium';
 import styles from '../styles';
 
@@ -7,19 +6,14 @@ function colorComplete(bool) {
   return bool ? { textDecoration: "line-through", color: '#79BD9A' } : {}
 }
 
-@Radium
-
-export default class ListItem extends Component {
-  constructor(props) {
-    super(props)
-  }
-  render() {
-    return (<div
-      style={Object.assign({}, styles.task, colorComplete(this.props.task.finished))}
-      key={this.props.task.id}
-      onClick={this.props.markComplete.bind(this, this.props.task)}
-    >
-      {this.props.task.name}
-    </div>);
-  }
+const ListItem = (props) => {
+  return (<div
+    style={Object.assign({}, styles.task, colorComplete(props.task.finished))}
+    key={props.task.id}
+    onClick={props.markComplete.bind(this, props.task)}
+  >
+    {props.task.name}
+  </div>);
 }
+
+module.exports = Radium(ListItem);
